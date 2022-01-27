22b9ed52e0613d067e0bd8d1971c7b0d14cc6ea1
Bjorn Bringert <bjorn@bringert.net>
2022-01-27 23:02:08 +0100

    Move code into a class to play with how to package the app

--- mutebutton.py
+++ mutebutton.py
@@ -4,29 +4,37 @@ import pygame.joystick
 
 MUTE_BUTTON = 1
 
+
 def button_pressed(button):
   if button == MUTE_BUTTON:
     print("Toggling mute in Microsoft Teams")
     os.system('osascript microsoft-teams-mute.applescript')
 
 
-running = True
+class JoystickWatcher:
+  def __init__(self, button_down=None):
+    self.button_down = button_down
+    self.running = True
+    pygame.init()
+    self.clock = pygame.time.Clock()
 
-pygame.init()
+    self.joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+    for joy in self.joysticks:
+      joy.init()
 
-clock = pygame.time.Clock()
+  def handle_joystick_events(self):
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        print("Quitting...")
+        self.running = False
+      elif event.type == pygame.JOYBUTTONDOWN:
+        if self.button_down:
+          self.button_down(event.button)
 
-joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
-for joy in joysticks:
-  joy.init()
+  def run(self):
+    while self.running:
+      self.handle_joystick_events()
+      self.clock.tick(20)
 
-while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      print("Quitting...")
-      running = False
-    elif event.type == pygame.JOYBUTTONDOWN:
-      button_pressed(event.button)
-  clock.tick(20)
-
-pygame.joystick.quit()
+if __name__ == '__main__':
+  JoystickWatcher(button_down=button_pressed).run()
