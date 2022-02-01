864db9b5ee5b9200d094b9fae6f2e205110d5ca8
Bjorn Bringert <bjorn@bringert.net>
2022-02-01 21:42:07 +0100

    Using logging instead of print() in actions.py

--- actions.py
+++ actions.py
@@ -1,5 +1,6 @@
+from logging import info
 import os
 
 def ms_teams_mute():
-  print("Toggling mute in Microsoft Teams...")
+  info("Toggling mute in Microsoft Teams...")
   os.system('osascript microsoft-teams-mute.scpt')
