8e19a7921822f744c8b63fe7fec0cbefa8bcd1f4
Bjorn Bringert <bjorn@bringert.net>
2022-02-03 09:11:39 +0100

    Move applescript to an external file for easy editing and testing

--- actions.py
+++ actions.py
@@ -2,11 +2,7 @@ import applescript
 from logging import info
 import os
 
-ms_teams_mute_scpt = applescript.AppleScript('''
-tell application "Microsoft Teams" to activate
-
-tell application "System Events" to keystroke "m" using {shift down, command down}
-''')
+ms_teams_mute_scpt = applescript.AppleScript(path="microsoft-teams-mute.applescript")
 
 def ms_teams_mute():
   ms_teams_mute_scpt.run()
